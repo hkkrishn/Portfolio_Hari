5d1411345cef53659f46c7485d5b88c4855890ab
Harikrishnan <hkkrishn563@gmail.com>
2020-09-10 16:17:36 -0700

    custom hook to generate portfolios

--- actions/portfolios.js
+++ actions/portfolios.js
@@ -11,34 +11,27 @@ const createPortfolio=(data)=> {
 }
 
 export const  useCreatePortfolio=()=>{
-
   //call useState to save request state vals
   const [reqState, setReqState] = useState({
     error: null,
     data: null,
     loading: false
   });
-
-
-
-
-
   const createPortfolioHandler = async (...data) => {
-
     //set the initial request object
     setReqState({error: null, data: null, loading: true});
     try {
       const json = await createPortfolio(...data);
-
       setReqState({error: null, data: json.data, loading: false});
     } catch(e) {
-
+      //check if there is an error resonse and response message else return custom message
       const message = (e.response && e.response.message) || 'There seems to be an error, something went wrong...';
+      //add the message to the request object.
       setReqState({error: message, data: null, loading: false});
     }
   }
-
   //return the array that return a function and the request object which
   //includes data
+  //spread the request object to grab all fieldsß
   return [createPortfolioHandler, {...reqState}]
 }
